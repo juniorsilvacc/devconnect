9a158e61d2da891c9dd7c5052c4aa5c50ca52c10
juniorsilvacc <juniorsilvacc@hotmail.com>
2021-07-31 05:03:21 -0300

    page signin

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,11 +1,39 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
+
 import './styles.css'
 
+import {isLogged} from '../../helpers/AuthHandler'
+
+import Logo from '../../assets/DevConnect.png';
+import Camera from '../../assets/camera.svg';
+
 function Header() {
+
+  let logged = isLogged();
+
   return (
-    <div>
-      
-    </div>
+    <header className="main-header">
+      <div className="header-content">
+        {!logged &&
+          <>
+            <Link to="/">
+              <img src={Logo} alt="DevConnect" />
+            </Link>
+          </>
+        }
+        {logged &&
+          <>
+            <Link to="/">
+              <img src={Logo} alt="DevConnect" />
+            </Link>
+            <Link to="/post">
+              <img src={Camera} alt="Postar publicação" />
+            </Link>
+          </>
+        }
+      </div>
+    </header>
   )
 }
 
--- frontend/src/pages/Signin/index.js
+++ frontend/src/pages/Signin/index.js
@@ -1,11 +1,37 @@
 import React from 'react'
+import {Link} from 'react-router-dom';
 import './styles.css';
+
+import Header from '../../components/Header'
+
+import HeroLogo from '../../assets/hero-img.png'
+import Logo from '../../assets/DevConnect.png'
 
 function Signin() {
   return (
-    <div>
-      <h1>Signin</h1>
-    </div>
+    <>
+      <Header/>
+      <div className="main-signin">
+        <div className="main-content">
+          <img className="imagemLogo" src={HeroLogo} alt="" />
+          <div className="form">
+            <form>
+              <img src={Logo} alt="" />
+              <input 
+                placeholder="Usuário"
+              />
+              <input 
+                placeholder="Senha"
+              />
+            </form>
+            <button className="buttonSignin">Entrar</button>
+            <div className="buttonTextRegister">
+              <p>Não tem uma conta? <Link>Cadastre-se</Link> </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
